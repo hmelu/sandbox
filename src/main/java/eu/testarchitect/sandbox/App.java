bf0b8d0d8ddd3e6fac5f1aa4a2c65baa2cf86076
hmelu <krystian.chmiel@gmail.com>
2019-10-15 20:19:18 +0200

    init project

---
+++ src/main/java/eu/testarchitect/sandbox/App.java
@@ -0,0 +1,13 @@
+package eu.testarchitect.sandbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
